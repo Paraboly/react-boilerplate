b48107e7485be575114243dc0e55b0645a48a3ce
SchemeSonic <haldun313@gmail.com>
2022-08-08 00:01:53 +0300

    code splitting for production

--- configs/webpack/prod.js
+++ configs/webpack/prod.js
@@ -1,6 +1,7 @@
+const HtmlMinimizerPlugin = require("html-minimizer-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const { merge } = require("webpack-merge");
 const { resolve } = require("path");
-
 const commonConfig = require("./common");
 
 module.exports = merge(commonConfig, {
@@ -11,5 +12,26 @@ module.exports = merge(commonConfig, {
     path: resolve(__dirname, "../../dist"),
     publicPath: "/"
   },
-  plugins: []
+  plugins: [],
+  optimization: {
+    minimize: true,
+    minimizer: [new CssMinimizerPlugin(), new HtmlMinimizerPlugin(), "..."],
+    runtimeChunk: "single",
+    splitChunks: {
+      chunks: "all",
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            const packageName = module.context.match(
+              /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+            );
+            if (packageName) {
+              return `npm.${packageName[1].replace("@", "")}`;
+            }
+          }
+        }
+      }
+    }
+  }
 });
